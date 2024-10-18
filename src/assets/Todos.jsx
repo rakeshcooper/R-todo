8e1d07895e717956e178ca26afcc103dbf51c094
rakeshcooper <rakeshmurali097@gmail.com>
2024-10-19 01:11:24 +0530

    update

--- src/assets/Todos.jsx
+++ src/assets/Todos.jsx
@@ -1,28 +1,54 @@
 import { useState , useRef} from "react"
 
 function Todos(){
-    const [datas, setDatas] = useState(["Drink water"])
+    const [datas, setDatas] = useState([])
     const [newdata, setNewdata] = useState()
-    const inputRef = useRef();
+    const [isedited, setNewisedited] = useState(true)
+    const inputRefadd = useRef();
+    const inputRefUpdate = useRef();
+    const editRef = useRef();
+    const updateRef = useRef();
     function inputhandler(e){
         setNewdata(e.target.value)
     }
     function addHandler(){
-        setDatas([...datas, newdata])
+        const data = {todo:newdata, rId:crypto.randomUUID()}
+        setDatas(datas => [...datas, data])
+        inputRefadd.current.value =""
+        
+        console.log(datas);   
     }
 
-    function deleteHandler(index){
-        setDatas(datas.filter((_,Findex) => index != Findex))
+    
+
+    function deleteHandler(rId){
+        setDatas(datas.filter((element) => element.rId != rId))
+        console.log("deleted",rId);
+        
+    }
+
+    function green(){
+        document.body.style.backgroundColor = "green"
+    }
+
+    function editHandler(){
+        // editRef.current.style.display = "none"
+        // updateRef.current.style.display = "block"
+    }
+
+    function updateHandler(){
+        // editRef.current.style.display = "block"
+        // updateRef.current.style.display = "none"
     }
 
     return(
         <>
             <div>
-                <input ref={inputRef} type="text" placeholder="Enter new task..."  onChange={(e) => inputhandler(e)}/>
-                <button onClick={() => addHandler()}>add</button>
+                <input ref={inputRefadd} type="text" placeholder="Enter new task..."  onChange={(e) => inputhandler(e)}/>
+                <button onClick={() => addHandler()}onDoubleClick={green}>add</button>
             </div>
             <ul>
-                {datas.map((list,index) => <li key={index}><span>{list}</span><button>update</button><button onClick={() => deleteHandler(index)}>delete</button></li>)}
+                {datas.map((list,index) => <li key={index}><span>{list.todo}</span><input ref={inputRefUpdate} type="text" name="updateInput"  /><span><button ref={editRef} onClick={editHandler}>Edit</button><button ref={updateRef} onClick={updateHandler}className="updateBtn">Update</button><button>Done</button><button onClick={() => deleteHandler(list.rId)}>delete</button></span></li>)}
             </ul>
         </>
     )
