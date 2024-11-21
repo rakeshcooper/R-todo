68907e900ab2730b011bfbe211c2c71eec8a4356
rakeshcooper <rakeshmurali097@gmail.com>
2024-11-21 19:52:12 +0530

    update

--- src/assets/Testing.jsx
+++ src/assets/Testing.jsx
@@ -2,21 +2,17 @@ import { v4 as uuidv4 } from 'uuid';
 import { useState,useEffect } from 'react';
 function Testing(){
 
-    const [data,setData] = useState([])
+    const [data,setData] = useState(JSON.parse(localStorage.getItem("TRItems") ?? "[]"))
     const [newdata, setNewdata] = useState()
 
     function addHandler(){
         setData(data => [...data,{ todo:newdata, rId:uuidv4(), isEdited: false, isChecked: false}])
-        
         console.log(data);
-        
     }
 
     function inputValue(e){
         setNewdata(e.target.value)
     }
-
-    
 
 
 
@@ -25,7 +21,13 @@ function Testing(){
         <h1>Todo</h1>
         <div><input type="text" placeholder='Please enter todo' onInput={(e) => inputValue(e)}/><button onClick={addHandler}>Add</button></div>
         <ul>
-
+            {data.map((datas,index) => (<li key={index}><span className={datas.isChecked ? "checked" : "unchecked"}>{datas.todo
+            }</span>
+            <span>{ datas.isEdited ? (<><input type="text" defaultValue={datas.todo} onInput={(e) => updateValue(e)}/><button onClick={() => updateHandler(datas.rId)}>update</button></>) : (<></>)}</span>
+            <button onClick={() => editHandler(datas.rId)}>{datas.isEdited  ? "cancel" : "edit"}</button>
+            <button onClick={() => deleteHandler(datas.rId)}>delete</button>
+            <button onClick={() => checkHandler(datas.rId)}>{datas.isChecked ? "undone" : "done"}</button>
+            </li>))}
         </ul>
         </>
     )
