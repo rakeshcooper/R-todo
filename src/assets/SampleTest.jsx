52605c2d8d2717cf10c2d92108f0c2b80180f83c
rakeshcooper <rakeshcooper10@gmail.com>
2025-04-09 19:17:58 +0530

    react learning updated

--- src/assets/SampleTest.jsx
+++ src/assets/SampleTest.jsx
@@ -6,20 +6,50 @@ const [data,setData] = useState([])
 const [newData, setnewData] = useState()
 
 function addHandler(){
-    setData( (data) => [{ todo:newData, id:rId, isEdited:false, isChecked: true},...data])
+    setData( (data) => [{ todo:newData, id:rId, isEdited:false, isChecked: false}, ...data])
     console.log(data)
 }
 
 function getInputValue(e){
     setnewData(e.target.value)
+
 }
 
 function editHandler(rId){
    const editData = data.map((datas) => {
        return datas.id == rId ? {...datas, isEdited :!datas.isEdited } : datas
     })
-    // console.log(rId);
     setData(editData)
+    console.log(editData);
+    
+}
+
+function getUpdateValue(e){
+    setnewData(e.target.value)
+}
+
+function updateHandler(rId){
+    const updatedData = data.map((datas) => {
+        return datas.id == rId ? {...datas, todo: newData, isEdited: !datas.isEdited} : datas
+    })
+    setData(updatedData)
+}
+
+function deleteHandler(rId){
+    const deletedData = data.filter((datas) => {
+        return datas.id != rId
+    })
+    setData(deletedData)
+    console.log("deleted");
+    
+}
+
+function checkedHandler(rId){
+    const checkedData = data.map((datas) => {
+        return datas.id == rId ? {...datas, isChecked: !datas.isChecked} : datas
+    })
+    console.log(checkedData);
+    setData(checkedData)
 }
 
 
@@ -29,13 +59,19 @@ function editHandler(rId){
     <div><input type="text" onInput={(e) => {getInputValue(e)}} name="todoInput" id="todoInput" /><button onClick={addHandler}>Add</button></div>
     <ul>
         {data.map((datas,index) => 
-        (<li key={index}>
-            <span>{datas.todo}</span>
+        <li key={index}>
+            <span className={datas.isChecked ? "checked" : "unchecked"}>{datas.todo}</span>
             <span>
-                {datas.isEdited ? (<><input type="text" name="" id="" /><button>update</button></>) : (<></>)}
+                {datas.isEdited ? (<><input type="text" defaultValue={datas.todo} onInput={(e) => {getUpdateValue(e)}} name="updateValue" id="updateValue" /><button onClick={() => updateHandler(datas.id)}>update</button></>) : (<></>)}
                 <button onClick={() => editHandler(datas.id)}>{datas.isEdited ? "cancel" : "edit"}</button>        
-                </span>
-        </li>))}
+            </span>
+            <span>
+                <button onClick={() => deleteHandler(datas.id)}>delete</button>
+            </span>
+            <span>
+                <button onClick={() => checkedHandler(datas.id)}>{datas.isChecked ? "undone" : "done"}</button>
+            </span>
+        </li>)}
     </ul>
     </>)
 }
