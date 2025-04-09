ba4c849bfc9e6d76ea206815037979e3b1997f83
rakeshcooper <rakeshcooper10@gmail.com>
2025-04-09 17:36:32 +0530

    sample update

--- src/assets/SampleTest.jsx
+++ src/assets/SampleTest.jsx
@@ -2,12 +2,25 @@ import { useState } from "react"
 
 function SampleTest(){
 const rId = crypto.randomUUID();    
-const [data,setData] = useState(["cooper","rakesh"])
+const [data,setData] = useState([])
+const [newData, setnewData] = useState()
+
+function addHandler(){
+    setData( (data) => [{ todo:newData, id:rId, isEdited:false, isChecked: true},...data])
+    console.log(data);
+    
+}
+
+function getInputValue(e){
+    setnewData(e.target.value)
+}
 
     return(<>
+    <h1>Todo Test</h1>
+    <div><input type="text" onInput={(e) => {getInputValue(e)}} name="todoInput" id="todoInput" /><button onClick={addHandler}>Add</button></div>
     <ul>
         {data.map((datas,index) => 
-        <li>{datas}</li>)}
+        <li key={index}>{datas.todo}</li>)}
     </ul>
     </>)
 }
